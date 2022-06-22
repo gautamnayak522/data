85fa75e02b8705aebef2570352dadca56a762126
Gautam Nayak <gautam.nayak@radixweb.local>
2022-06-22 19:53:05 +0530

    course

---
+++ SQL/SQL Course/SQLQuery4.sql
@@ -0,0 +1,12 @@
+/*               Syntax of DISTINT Keyword
+
+		SELECT 
+				DISTINCT
+						 NAME OF COLUMNS [ in case you have multiple COLUMNS than place , between Columns]
+        FROM
+			TABLE NAME          
+*/
+
+
+
+select distinct Employee_Last_Name from Employee_Information
